345d73057a67bb364d4751ad72c78a9ec120ceab
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-09-20 12:21:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
-    //Write your code here
+
+	const newRow = document.createElement("tr");
+
+	newRow.innerHTML=`<td>New cell1</td>
+					<td>New cell2</td>
+ `;
+	sampleTable.prepend(newRow);
   
   
 }
